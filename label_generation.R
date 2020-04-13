ff2af2d4346e41d25bf0738d08841d778fa6ad07
Laurence James-Woodley <mr.lawjames@gmail.com>
2020-04-13 15:59:08 -0400

    use baseline fileds

--- label_generation.R
+++ label_generation.R
@@ -25,10 +25,21 @@ email_to <- unlist(strsplit(Sys.getenv("EMAIL_TO")," "))
 email_cc <- unlist(strsplit(Sys.getenv("EMAIL_CC")," "))
 email_subject <- paste(Sys.getenv("EMAIL_SUBJECT"), appt_date)
 
+# get baseline fields
+fields_from_baseline <- c("ce_firstname", "ce_lastname", "patient_dob")
+
+baseline_records <- redcap_read_oneshot(redcap_uri = 'https://redcap.ctsi.ufl.edu/redcap/api/',
+                                        token = Sys.getenv("API_TOKEN"))$data %>%
+  filter(redcap_event_name == 'baseline_arm_1' & !is.na(ce_firstname)) %>%
+  select(record_id, ce_firstname, ce_lastname, patient_dob) %>%
+  mutate(ce_firstname = str_to_title(ce_firstname),
+         ce_lastname = str_to_title(ce_lastname))
+
 test_tube_label <- redcap_read_oneshot(redcap_uri = 'https://redcap.ctsi.ufl.edu/redcap/api/',
                                        token = Sys.getenv("API_TOKEN"))$data %>%
-  select(research_encounter_id, ce_firstname, ce_lastname, patient_dob,
+  select(research_encounter_id,record_id, redcap_event_name,
          site_short_name, site_long_name, test_date_and_time) %>%
+  left_join(baseline_records, by = "record_id") %>%
   mutate(ce_firstname = str_to_title(ce_firstname),
          ce_lastname = str_to_title(ce_lastname),
          subject_id = paste(ce_firstname, ce_lastname, patient_dob)) %>%
