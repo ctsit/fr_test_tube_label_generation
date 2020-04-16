72708551a895c1dd68e4208c67c480a31b16c4a8
Laurence James-Woodley <mr.lawjames@gmail.com>
2020-04-16 13:54:46 -0400

    add pk younge

--- functions.R
+++ functions.R
@@ -1,7 +1,7 @@
 get_test_tube_label <- function(){
 
   records <- redcap_read_oneshot(redcap_uri = 'https://redcap.ctsi.ufl.edu/redcap/api/',
-                                 token = Sys.getenv("API_TOKEN"))$data
+                                 token = Sys.getenv("FR_API_TOKEN"))$data
 
   baseline_records <- records %>%
     filter(redcap_event_name == 'baseline_arm_1' & !is.na(ce_firstname)) %>%
@@ -28,4 +28,30 @@ add_new_row <- function(per_site_df, ...){
           research_encounter_id = unique(per_site_df$site_short_name),
           site_short_name = unique(per_site_df$site_short_name),
           ...)
+}
+
+get_pk_test_tube_label <- function(){
+  records <- redcap_read_oneshot(redcap_uri = 'https://redcap.ctsi.ufl.edu/redcap/api/',
+                                 token = Sys.getenv("PK_API_TOKEN"))$data
+
+  baseline_records <- records %>%
+    filter(redcap_event_name == 'baseline_arm_1' & !is.na(ce_firstname)) %>%
+    select(record_id, ce_firstname, ce_lastname, patient_dob,
+           "Finger_Prick_OK" = icf_fingerstick) %>%
+    mutate(ce_firstname = str_to_title(ce_firstname),
+           ce_lastname = str_to_title(ce_lastname),
+           subject_id = paste(ce_firstname, ce_lastname,"\n", patient_dob))
+
+  test_tube_label <- records %>%
+    select(research_encounter_id, record_id, redcap_event_name,
+           site_short_name, site_long_name, test_date_and_time) %>%
+    filter(as_date(test_date_and_time) == appt_date) %>%
+    left_join(baseline_records, by = "record_id") %>%
+    select(-record_id) %>%
+    add_column("Barcode Confirmed" = "",
+               "Picture ID Confirmed" = "",
+               "Assent-Consent Confirmed" = "") %>%
+    arrange(test_date_and_time, ce_lastname)
+
+  return(test_tube_label)
 }
---
+++ pk_yonge_label_generation.R
@@ -0,0 +1,110 @@
+library(tidyverse)
+library(sendmailR)
+library(dotenv)
+library(devtools)
+# install dev version of baRcodeR if not previously installed
+# devtools::install_github('ropensci/baRcodeR')
+library(baRcodeR)
+library(REDCapR)
+library(lubridate)
+
+source("functions.R")
+
+# set the timezone
+Sys.setenv(TZ = Sys.getenv("TIME_ZONE"))
+
+# When script is run Mon-Fri appt_date is the next day
+# When script is run on Sat appt_day is Monday
+appt_date <- case_when(
+  wday(now()) == 7 ~ today() + 2,
+  TRUE ~ today() + 1
+)
+
+# email credentials
+email_server <- list(smtpServer = Sys.getenv("SMTP_SERVER"))
+email_from <- Sys.getenv("PK_EMAIL_FROM")
+email_to <- unlist(strsplit(Sys.getenv("PK_EMAIL_TO")," "))
+email_cc <- unlist(strsplit(Sys.getenv("PK_EMAIL_CC")," "))
+email_subject <- paste(Sys.getenv("PK_EMAIL_SUBJECT"), appt_date)
+
+test_tube_label <- get_pk_test_tube_label()
+
+appt_counts <- test_tube_label %>%
+  count(site_long_name) %>%
+  add_row(site_long_name = "Total", n = nrow(test_tube_label)) %>%
+  unite("Counts", sep = " = ")
+
+# create folder to store output
+output_dir <- paste0("pk_covid19_", appt_date)
+dir.create(output_dir, recursive = T)
+
+# create per site roster
+test_tube_label %>%
+  select(-subject_id) %>%
+  write.csv(paste0(output_dir, "/pk_yonge_roster_", appt_date, ".csv"),
+            na = "", row.names = F)
+
+
+sites <- unique(test_tube_label$site_short_name)
+# create per site barcode pdfs
+for (site in sites){
+
+  per_site_df <- test_tube_label %>%
+    select(research_encounter_id, site_short_name, subject_id) %>%
+    filter(site_short_name == site)
+
+  if(nrow(per_site_df) <= 19){
+    per_site_df <- per_site_df %>%
+      add_new_row(.before = 1) %>%
+      slice(rep(1:n(), each = 4))
+  } else if (nrow(per_site_df) <= 38){
+    per_site_df <- per_site_df %>%
+      add_new_row(.before = 1) %>%
+      add_new_row(.before = 21) %>%
+      slice(rep(1:n(), each = 4))
+  } else {
+    per_site_df <- per_site_df %>%
+      add_new_row(.before = 1) %>%
+      add_new_row(.before = 21) %>%
+      add_new_row(.before = 41) %>%
+      slice(rep(1:n(), each = 4))
+  }
+
+  custom_create_PDF(Labels = per_site_df$research_encounter_id,
+                    alt_text = per_site_df$subject_id,
+                    type = "matrix",
+                    label_height = 0.3,
+                    denote = c("\n","\n"),
+                    Fsz = 5,
+                    trunc = T,
+                    y_space = 0.5,
+                    ErrCorr = "Q",
+                    name = paste0(output_dir, "/", site,
+                                  '_pk_covid_test_tube_labels_',
+                                  appt_date))
+}
+
+
+# Zip the reports generated
+zipfile_name = paste0(output_dir, ".zip")
+zip(zipfile_name, output_dir)
+
+# attach the zip file and email it
+attachment_object <- mime_part(zipfile_name, zipfile_name)
+body <- paste0("The attached files include labels to be printed for the PK Yonge COVID-19 project.",
+               " These labels are designed for the serum tubes and swab collection kits to be used at the collection sites.",
+               " These labels should be printed and packaged with the serum and swab kits for their respective sites.",
+               " The attached files were generated on ", now(), ".",
+               "\n\nNumber of appts for ", appt_date, ": ", str_remove_all(appt_counts,"[[:punct::]]")
+)
+
+body_with_attachment <- list(body, attachment_object)
+
+# send the email with the attached output file
+sendmail(from = email_from, to = email_to, cc = email_cc,
+         subject = email_subject, msg = body_with_attachment,
+         control = email_server)
+
+# uncomment to delete output once on tools4
+# unlink(zipfile_name, recursive = T)
+# unlink(output_dir, recursive = T)
